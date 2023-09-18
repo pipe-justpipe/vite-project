163d069cd094c1c387f63dd542d11a58eecc5e5d
pipe-justpipe <aremopipeloluwa@gmail.com>
2023-09-18 17:53:21 +0100

    commit

--- src/Effect.jsx
+++ src/Effect.jsx
@@ -5,7 +5,7 @@ const Effect = () => {
 
     useEffect(() => {////ygiug hbvj jggcg  yutfty uftyued
         console.log("first")
-    }, [])
+    }, [])////////////
     //use effect takes in an anonymous function and an array dependency used to call API's and setTimers, it runs asynchronously, it crestes an effect without sideEffect
     //if array [] is empty, it is equal to componentDidMount in class component life cycle method
     //it takes in an anonymous function and an array dependency
